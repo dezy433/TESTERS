28e1eafa7713bb68a654d1cc1f4589a6cfb045f8
dezy433 <75358865+dezy433@users.noreply.github.com>
2022-03-09 21:56:48 +0000

    Update main.py

--- main.py
+++ main.py
@@ -4,71 +4,72 @@ if __name__ == '__name__':
  def id_func():
     while True:
         id_str = input("Employee ID")
-    if id_str.isdigit():
-        id_int = int(id_str)
-    else:
-        print("Add numbers")
+        if id_str.isdigit():
+         id_int = int(id_str)
+        else:
+         print("Add numbers")
 
 
 
  def first_name_func():
     while True:
         name = input("name")
-    if len(name.strip()) > 2:
+        if len(name.strip()) > 2:
             break
     return name
 
  def last_name_func():
     while True:
         l_name = input("last name")
-    if len(l_name.strip()) > 2:
-            break
+        if len(l_name.strip()) > 2:
+           break
     return l_name
  def birth_year_func():
     while True:
         birth_year_str = (input("birth year"))
-    if birth_year_str.isdigit():
-        birth_year = int(birth_year_str)
-    if (birth_year >= 1900) and (birth_year <= 2004):
-            break
-    else:
-        print("Years ranging from 1990-2004")
+        if birth_year_str.isdigit():
+         birth_year = int(birth_year_str)
+         if (birth_year >= 1900) and (birth_year <= 2004):
+          break
+        else:
+         print("Years ranging from 1990-2004")
     return birth_year
 
  def birth_month_func():
     while True:
         birth_month_str = (input("birth month"))
-    if birth_month_str.isdigit():
-        birth_month = int(birth_month_str)
-    if (birth_month >= 1) and (birth_month <= 12):
-        print("Please enter as a number")
+        if birth_month_str.isdigit():
+         birth_month = int(birth_month_str)
+         if (birth_month >= 1) and (birth_month <= 12):
+          print("Please enter as a number")
 
 
  def bday_func():
     while True:
-        bday_str = (input("birth Day"))
-    if bday_str.isdigit():
-        bday = int(bday_str)
-    if (bday >= 1) and (bday <= 31):
-        break
+         bday_str = input("birth Day")
+         if bday_str.isdigit():
+             bday = int(bday_str)
+         if(bday >= 1) and (bday <= 31):
     return bday
-else:
-    print("Please enter as a number")
+        else:
+            print("Please enter as a number")
 
 
  def position_func():
     while True:
         position_str = input("Position")
-    if len(position_str.strip()) > 0:
-            position = str(position_str)
-
+        if len(position_str.strip()) > 0:
+         position = str(position_str)
+         return position
+    else:
+        print("Please enter your position")
 
  def graduate_func():
     answers =["yes","no","Yes","No"]
     while True:
         graduate_str = input("Have you attended univerisity ? ")
-    if graduate_str in answers:
-        return graduate_str
+        if graduate_str in answers:
+         return graduate_str
     else:
         print("Please answer 'Yes' or 'No'")
 
@@ -95,14 +96,4 @@ else:
         "Position": pos,
         "Univeristy": grad
     }
-    return employee
-def remove_employee():
-    first_name = first_name_func()
-    last_name = last_name_func()
-    bday = bday_func()
-    birth_month = birth_month_func()
-    birth_year = birth_year_func()
-    ids = id_func()
-    pos = position_func()
-    grad = graduate_func()
-
+        return employee
